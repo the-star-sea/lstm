99b3d4cfd10806f749437bf7c1a0453f7cdb3e30
stone <11911611@mail.sustech.edu.cn>
2021-12-21 20:15:06 +0800

    haha

--- src/model.py
+++ src/model.py
@@ -2,24 +2,32 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+from torch.autograd import Variable
 class TextGenerator(nn.ModuleList):
     def __init__(self, args, vocab_size):
         super(TextGenerator, self).__init__()
 
         self.batch_size = args.batch_size
-        self.hidden_dim = args.hidden_dim
+        self.hidden_size = args.hidden_dim
+        self.attention_size=int(args.hidden_dim/2)
         self.input_size = vocab_size
         self.num_classes = vocab_size
         self.sequence_len = args.window
-
+        self.layer_size=args.layer_size
+        self.embed_size = args.embed_size
         # Embedding layer
-        self.embedding = nn.Embedding(self.input_size, self.hidden_dim)
-        self.lstm = nn.LSTM(self.hidden_dim, self.hidden_dim, num_layers=3,
+        self.embedding = nn.Embedding(self.input_size, self.embed_size)
+        self.lstm = nn.LSTM(self.embed_size, self.hidden_size, num_layers=self.layer_size,
                             batch_first=False,dropout=0.25, bidirectional=True)
-        self.fc1 = nn.Linear(2*self.hidden_dim,2048)
-        self.fc2 = nn.Linear(2048,4096)
-        self.fc3 = nn.Linear(4096,self.num_classes)
+        self.layer_size =self.layer_size*2
+
+        self.fc = nn.Linear(self.hidden_size,self.num_classes)
+        self.attention_weights_layer = nn.Sequential(
+            nn.Linear(self.hidden_size, self.hidden_size),
+            nn.ReLU(inplace=True)
+        )
+
+
 
 
     def forward(self, x, hidden=None):
@@ -28,15 +36,28 @@ class TextGenerator(nn.ModuleList):
         out = self.embedding(x)
         out = out.view(self.sequence_len, x.size(0), -1)
         if hidden is None:
-            h_0 = x.data.new(2*3, self.batch_size, self.hidden_dim).fill_(0).float().to('cuda')
-            c_0 = x.data.new(2*3, self.batch_size, self.hidden_dim).fill_(0).float().to('cuda')
+            h_0 = x.data.new(self.layer_size, self.batch_size, self.hidden_size).fill_(0).float().to('cuda')
+            c_0 = x.data.new(self.layer_size, self.batch_size, self.hidden_size).fill_(0).float().to('cuda')
         else:
             h_0, c_0 = hidden
-        # print(out.shape)
-        output, hidden = self.lstm(out, (h_0, c_0))#hidden 是h,和c 这两个隐状态
-        # output=output.view(self.sequence_len * self.batch_size, -1)
-        output = torch.relu(self.fc1(output))
-        output = torch.relu(self.fc2(output))
-        output = self.fc3(output)
-        # output = output.reshape(self.batch_size* self.sequence_len, -1)
-        return output,hidden
+
+        out, (h_n, c_n) = self.lstm(out, (h_0, c_0))#hidden 是h,和c 这两个隐状态
+        (forward_out, backward_out) = torch.chunk(out, 2, dim=2)
+        out = forward_out + backward_out  # [seq_len, batch, hidden_size]
+        out = out.permute(1, 0, 2)  # [batch, seq_len, hidden_size]
+
+        # 为了使用到lstm最后一个时间步时，每层lstm的表达，用h_n生成attention的权重
+        h_n = h_n.permute(1, 0, 2)  # [batch, num_layers * num_directions,  hidden_size]
+        h_n = torch.sum(h_n, dim=1)  # [batch, 1,  hidden_size]
+        h_n = h_n.squeeze(dim=1)  # [batch, hidden_size]
+
+        attention_w = self.attention_weights_layer(h_n)  # [batch, hidden_size]
+        attention_w = attention_w.unsqueeze(dim=1)  # [batch, 1, hidden_size]
+
+        attention_context = torch.bmm(attention_w, out.transpose(1, 2))  # [batch, 1, seq_len]
+        softmax_w = F.softmax(attention_context, dim=-1)  # [batch, 1, seq_len],权重归一化
+
+        x = torch.bmm(softmax_w, out)  # [batch, 1, hidden_size]
+        x = x.squeeze(dim=1)
+        out = self.fc(x)
+        return out,(h_n, c_n)
